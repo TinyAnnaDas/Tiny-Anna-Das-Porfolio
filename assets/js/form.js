6a6383872e86eea5a100c42496dc1c0de60c6414
Tiny P Christhudas <tinypchristhudas@gamil.com>
2022-09-05 15:14:07 +0530

    Final commit

---
+++ assets/js/form.js
@@ -0,0 +1,60 @@
+const submitform = document.getElementById('submit-form');
+const fname = document.getElementById('fname');
+const email = document.getElementById('email');
+const project = document.getElementById('project');
+
+submitform.addEventListener('submit', e => {
+  e.preventDefault();
+
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+  const fnameValue = fname.value.trim();
+  const emailValue = email.value.trim();
+  const projectValue = project.value.trim();
+
+
+  if (fnameValue === '') {
+    setError(fname, 'Name is required');
+  } else {
+    setSuccess(fname);
+  }
+
+  if (emailValue === '') {
+    setError(email, 'Email is required');
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, 'Provide a valid email address');
+  } else {
+    setSuccess(email);
+  }
+
+  if (projectValue === '') {
+    setError(project, 'Tell me about the project please');
+  } else {
+    setSuccess(project);
+  }
+};
