8212d618fa5f32539c0c51117e4a73896cc8d1f1
Tiny P Christhudas <tinypchristhudas@gamil.com>
2022-09-06 07:52:00 +0530

    validatio mistake figured

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,60 +1,54 @@
-const submitform = document.getElementById('submit-form');
-const fname = document.getElementById('fname');
-const email = document.getElementById('email');
-const project = document.getElementById('project');
 
-submitform.addEventListener('submit', e => {
-  e.preventDefault();
-
-  validateInputs();
-});
-
-const setError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector('.error');
-
-  errorDisplay.innerText = message;
-  inputControl.classList.add('error');
-  inputControl.classList.remove('success')
-}
-
-const setSuccess = element => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector('.error');
-
-  errorDisplay.innerText = '';
-  inputControl.classList.add('success');
-  inputControl.classList.remove('error');
-};
-
-const isValidEmail = email => {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
-const validateInputs = () => {
-  const fnameValue = fname.value.trim();
-  const emailValue = email.value.trim();
-  const projectValue = project.value.trim();
+    var nameError = document.getElementById('name-error');
+    var emailError = document.getElementById('email-error');
+    var submitError = document.getElementById('submit-error');
 
 
-  if (fnameValue === '') {
-    setError(fname, 'Name is required');
-  } else {
-    setSuccess(fname);
-  }
+    function validateName() {
+      var name = document.getElementById('contact-name').value.trim();
 
-  if (emailValue === '') {
-    setError(email, 'Email is required');
-  } else if (!isValidEmail(emailValue)) {
-    setError(email, 'Provide a valid email address');
-  } else {
-    setSuccess(email);
-  }
+      if (name.length <= 2) {
+        nameError.innerHTML = 'Name is Required';
+        nameError.style.color = 'red'
+        return false;
+      }
 
-  if (projectValue === '') {
-    setError(project, 'Tell me about the project please');
-  } else {
-    setSuccess(project);
-  }
-};
+      if (!name.match(/^[A-Za-z][A-Za-z]*$/)) {
+        nameError.innerHTML = 'invalid name';
+        nameError.style.color = 'red'
+        return false;
+      }
+
+      nameError.innerHTML = 'Name is valid';
+      nameError.style.color = 'green'
+      return true;
+    }
+    function validatesubmit() {
+      var submit = document.getElementById('contact-subject').value;
+      if (submit.length == 0) {
+        submitError.innerHTML = 'subject is Required';
+        submitError.style.color = 'red'
+        return false;
+      }
+    }
+
+    function validateEmail() {
+      var email = document.getElementById('contact-email').value;
+      if (email.length == 0) {
+        emailError.innerHTML = 'Email is Required';
+        emailError.style.color = 'red'
+        return false;
+      }
+
+      if (!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        emailError.innerHTML = 'Email is Invalid';
+        emailError.style.color = 'red'
+        return false;
+      }
+      emailError.innerHTML = 'Email is valid';
+      emailError.style.color = 'green'
+      return true;
+    }
+    
+
+    
